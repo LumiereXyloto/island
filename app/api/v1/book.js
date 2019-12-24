0768b502f74fe3f0783f4f2f380bfea86b37a450
LumiereXyloto <740207968@qq.com>
2019-12-24 21:34:36 +0800

    feat: 🎸 获取图书详情

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,4 +1,9 @@
 const Router = require('koa-router')
+const { Book } = require('../../models/book')
+const {
+  // SearchValidator,
+  PositiveIntegerValidator,
+} = require('../../validators/validator')
 const router = new Router({
   prefix: '/v1/book'
 })
@@ -8,6 +13,12 @@ const { HotBook } = require('../../models/hot-book')
 router.get('/hot_list', async ctx => {
   const books = await HotBook.getAll()
   ctx.body = books
+})
+
+router.get('/:id/detail', async ctx => {
+  const v = await new PositiveIntegerValidator().validate(ctx)
+  const book = await Book.detail(v.get('path.id'))
+  ctx.body = book
 })
 
 module.exports = router
